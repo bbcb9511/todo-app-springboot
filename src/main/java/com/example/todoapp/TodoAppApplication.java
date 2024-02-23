7f0704e5affeecd91edbe8167a2a1da20b651aee
murayama <bbcb9511@gmail.com>
2024-02-23 22:00:30 +0900

    クライアントとバックエンドの簡易連携確認済み

--- src/main/java/com/example/todoapp/TodoAppApplication.java
+++ src/main/java/com/example/todoapp/TodoAppApplication.java
@@ -3,11 +3,12 @@ package com.example.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
 @SpringBootApplication
 public class TodoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
-
 }
